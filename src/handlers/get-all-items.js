8d6905c415293fb4bec30d759d0b40d4f27667f4
fatsoap <g8sigma@icloud.com>
2022-02-15 18:44:12 +0800

    remove useless code for testing

--- src/handlers/get-all-items.js
+++ src/handlers/get-all-items.js
@@ -16,44 +16,16 @@ const docClient = new dynamodb.DocumentClient(
       }
 );
 
-var params = {
-  TableName: 'SimpleTable',
-};
-let data;
-docClient
-  .scan(params)
-  .promise()
-  .then((data) => console.log(data));
-
-/**
- * A simple example includes a HTTP get method to get all items from a DynamoDB table.
- */
 exports.getAllItemsHandler = async (event) => {
   if (event.httpMethod !== 'GET') {
     throw new Error(
       `getAllItems only accept GET method, you tried: ${event.httpMethod}`
     );
   }
-  // All log statements are written to CloudWatch
-  console.info('received:', event);
-
-  // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
-  // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
-  var params = {
-    TableName: tableName,
-  };
-  let data;
-  try {
-    data = await docClient.scan(params).promise();
-  } catch (err) {
-    data = { Item: JSON.stringify(err) };
-  }
-  const items = data.Item;
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(items),
+    body: JSON.stringify({ data: 'Health Check Pass' }),
   };
 
   // All log statements are written to CloudWatch
--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -12,45 +12,16 @@ const docClient = new dynamodb.DocumentClient(
       }
 );
 
-var params = {
-  TableName: 'SimpleTable',
-  Key: { id: '123' },
-};
-let data;
-docClient
-  .get(params)
-  .promise()
-  .then((data) => console.log(data));
-
 exports.getByIdHandler = async (event) => {
   if (event.httpMethod !== 'GET') {
     throw new Error(
       `getMethod only accept GET method, you tried: ${event.httpMethod}`
     );
   }
-  // All log statements are written to CloudWatch
-  console.info('received:', event);
-
-  // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
-  const id = event.pathParameters.id;
-
-  // Get the item from the table
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
-  var params = {
-    TableName: tableName,
-    Key: { id: id },
-  };
-  let data;
-  try {
-    data = await docClient.get(params).promise();
-  } catch (err) {
-    data = { Item: 'error' };
-  }
-  const item = data.Item;
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify(item),
+    body: JSON.stringify({ data: 'Health Check Pass' }),
   };
 
   // All log statements are written to CloudWatch
