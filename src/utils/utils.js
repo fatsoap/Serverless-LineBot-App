686658a1bc27dc657ce4bc81d3f8b731f5aa2515
fatsoap <g8sigma@icloud.com>
2022-02-18 18:11:41 +0800

    export get time function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 /** YYYY-MM-DD */
-function getCurrentTimeString() {
+exports.getCurrentTimeString = function() {
   let d = new Date();
   d.setUTCHours(d.getUTCHours() + 8);
   return `${d.getUTCFullYear()}-${d.getUTCMonth() + 1}-${d.getUTCDate()}`;
