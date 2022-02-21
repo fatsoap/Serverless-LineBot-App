cb1279cacd96431d268658f2e4f506c5f2823b7d
fatsoap <g8sigma@icloud.com>
2022-02-21 22:17:24 +0800

    fix stage check

--- src/handlers/put_products.js
+++ src/handlers/put_products.js
@@ -5,7 +5,7 @@ const dynamodb = require('aws-sdk/clients/dynamodb');
 const { getCurrentTimeString } = require('../utils/utils');
 
 const docClient = new dynamodb.DocumentClient(
-  process.env.STAGE
+  process.env.STAGE === 'PROD'
     ? {}
     : {
         region: 'ap-northeast-1',
